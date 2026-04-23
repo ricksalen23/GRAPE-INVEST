e862ae4815442a792e122042471b9ac91f37ee31
ricksalen23 <empresaexpert2@gmail.com>
2026-04-23 12:27:31 -0300

    fix nav categorias cache v9 manifest

--- sw.js
+++ sw.js
@@ -1,35 +1,27 @@
-const CACHE = 'grape-invest-v8';
-const ASSETS = [
-  '/',
-  '/index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'
-];
+const CACHE = 'grape-invest-v9';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {})
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return fetch(e.request).then(res => {
-        if (!res || res.status !== 200 || res.type === 'opaque') return res || cached;
+    fetch(e.request).then(res => {
+      if (res && res.status === 200) {
         const clone = res.clone();
         caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        return res;
-      }).catch(() => cached || caches.match('/index.html'));
-    })
+      }
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
